1f0eb4f0965593869db05677c23e52f6ee14662c
Jessica Oliveira <jessie_oliveira@icloud.com>
2018-07-11 12:25:05 -0400

    Added: Alamofire dependency and Registration API request
    Installed: Alamofire and pods.
    Added: Registration API requests.
    Tested: New users are successfully added to the database.

--- OLIVEIRAJESSICA_DAILYQUESTS/OLIVEIRAJESSICA_DAILYQUESTS/ViewController.swift
+++ OLIVEIRAJESSICA_DAILYQUESTS/OLIVEIRAJESSICA_DAILYQUESTS/ViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2018 Jessica. All rights reserved.
 //
 
+import Alamofire
 import UIKit
 
 class ViewController: UIViewController
@@ -14,6 +15,10 @@ class ViewController: UIViewController
     @IBOutlet weak var reg_username_txtField: UITextField!
     @IBOutlet weak var reg_email_txtField: UITextField!
     @IBOutlet weak var reg_pw_txtField: UITextField!
+    @IBOutlet weak var msg: UILabel!
+    
+    // Define a variable that holds the url for accessing the register API
+    let register_url = "http://dailyquests.club/JessyServer/v1/register.php"
     
     override func viewDidLoad()
     {
@@ -28,6 +33,29 @@ class ViewController: UIViewController
         super.didReceiveMemoryWarning()
     }
 
+    @IBAction func registerUser(_ sender: UIButton)
+    {
+        // Creating parameters for the post request
+        let parameters: Parameters = ["username":reg_username_txtField.text!, "email":reg_email_txtField.text!, "password":reg_pw_txtField.text!]
+        
+        // Sending http post request
+        Alamofire.request(register_url, method: .post, parameters: parameters).responseJSON
+        { (response) in
+            
+            print(response)
+            
+            // Getting the json value from the server
+            if let result = response.result.value
+            {
+                // Converting it as NSDictionary
+                let jsonData = result as! NSDictionary
+                
+                // Displaying the message in label for testing purposes
+                self.msg.text = jsonData.value(forKey: "message") as! String?
+            }
+        }
+
+    }
     
 
 }
